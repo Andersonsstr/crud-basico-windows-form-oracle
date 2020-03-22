d9a921a2e0fa1a7a853a319abcd1ca6ca1c3d953
Rodrigo <rodrigo.wlk@gmail.com>
2020-03-22 19:27:40 -0300

    Crud funcionando, pendente inserir imagem

--- CrudBasico Windows Form/src/Main.cs
+++ CrudBasico Windows Form/src/Main.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace CrudBasico_Windows_Form
@@ -9,6 +10,8 @@ namespace CrudBasico_Windows_Form
     public partial class Main : Form
     {
         List<TextBox> campos = new List<TextBox>();
+        string idSelected;
+        OpenFileDialog ofSelecionaImagem;
 
         public void adicionaCampos()
         {
@@ -18,12 +21,19 @@ namespace CrudBasico_Windows_Form
             campos.Add(txtIP);
         }
 
-
         public Main()
         {
             InitializeComponent();
             atualizarMaquinas();
             adicionaCampos();
+            limparCelulas();
+
+            ofSelecionaImagem = new OpenFileDialog()
+            {
+                FileName = "",
+                Filter = "Arquivos de Imagem (*.jpg)|*.jpg",
+                Title = "Selecione uma Imagem"
+            };
         }
 
         private void btnCadastrar_Click(object sender, System.EventArgs e)
@@ -33,10 +43,24 @@ namespace CrudBasico_Windows_Form
             {
                 if (validaCampos())
                 {
-                    ConexaoOracle.ExecutaComando($"insert into maquina values ('', '{txtHostname.Text}', '{txtIP.Text}', '{txtModelo.Text}', '{txtSetor.Text}', '', sysdate)");
-                    MessageBox.Show("Máquina cadastrada com sucesso", "Cadastrado!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    string efetuado = "";
+                    if (btnCadastrar.Text == "Cadastrar")
+                    {
+                        ConexaoOracle.ExecutaComando($"insert into maquina values ('', '{txtHostname.Text}', '{txtIP.Text}', '{txtModelo.Text}', '{txtSetor.Text}', '', sysdate)");
+                        efetuado = "cadastrada";
+                    }
+
+                    else if (btnCadastrar.Text == "Editar")
+                    {
+                        ConexaoOracle.ExecutaComando($"update maquina set hostname = '{txtHostname.Text}', ip = '{txtIP.Text}', modelo = '{txtModelo.Text}', setor = '{txtSetor.Text}' where id = {idSelected}");
+                        efetuado = "editada";
+                        btnVoltar.PerformClick();
+                    }
+
+                    MessageBox.Show("Máquina " + efetuado + " com sucesso", "Cadastrado!", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     limparCampos();
                     atualizarMaquinas();
+                    limparCelulas();
                 }
 
             }
@@ -50,7 +74,7 @@ namespace CrudBasico_Windows_Form
 
         private void atualizarMaquinas()
         {
-            dgDados.DataSource = ConexaoOracle.RetornaDados("SELECT * FROM MAQUINA");
+            dgDados.DataSource = ConexaoOracle.RetornaDados("SELECT M.ID, M.HOSTNAME, M.IP, M.MODELO, M.SETOR, M.DATA_CADASTRO FROM MAQUINA M");
         }
 
         private Boolean validaCampos()
@@ -72,6 +96,61 @@ namespace CrudBasico_Windows_Form
             for (int i = 0; i < campos.Count; i++)
             {
                 campos[i].Clear();
+            }
+        }
+
+        private void dgDados_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtIP.Text = dgDados.Rows[dgDados.SelectedCells[0].RowIndex].Cells["IP"].Value.ToString();
+            txtHostname.Text = dgDados.Rows[dgDados.SelectedCells[0].RowIndex].Cells["HOSTNAME"].Value.ToString();
+            txtSetor.Text = dgDados.Rows[dgDados.SelectedCells[0].RowIndex].Cells["SETOR"].Value.ToString();
+            txtModelo.Text = dgDados.Rows[dgDados.SelectedCells[0].RowIndex].Cells["MODELO"].Value.ToString();
+            idSelected = dgDados.Rows[dgDados.SelectedCells[0].RowIndex].Cells["ID"].Value.ToString();
+            btnCadastrar.Text = "Editar";
+            btnDeletar.Visible = true;
+            btnVoltar.Visible = true;
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            btnDeletar.Visible = false;
+            btnVoltar.Visible = false;
+            btnCadastrar.Text = "Cadastrar";
+            limparCampos();
+            dgDados.CurrentCell = null;
+            limparCelulas();
+
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            ConexaoOracle.ExecutaComando($"delete from maquina where ID =" + idSelected);
+            MessageBox.Show("Máquina Deletada com sucesso", "Deletada!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            limparCampos();
+            atualizarMaquinas();
+            btnVoltar.PerformClick();
+            limparCelulas();
+        }
+
+        private void limparCelulas()
+        {
+            dgDados.CurrentCell = null;
+            dgDados.ClearSelection();
+        }
+
+        private void lblCarregaFoto_Click(object sender, EventArgs e)
+        {         
+            if (ofSelecionaImagem.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var caminhoImagem = ofSelecionaImagem.FileName;
+                    pbFoto.Image = Image.FromFile(caminhoImagem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao carregar arquivo:" + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
     }
